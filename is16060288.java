b3effee10e139eb862194a4a2bc27b7b53b7fcc3
ZHIKANG TIAN <sdutbruce@outlook.com>
2017-02-22 11:44:47 +0000

    Update is16060288.java

--- is16060288.java
+++ is16060288.java
@@ -1,21 +1,79 @@
 import java.util.Scanner;
+import java.io.File;
+import java.io.PrintWriter;
+
+/*import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+*/
 
 public class is16060288{
 	
 	public static void main(String []args){
-		//new ParameterSetter();
+		ParameterGetter.getUserInput();
+		new GenerateData(	ParameterGetter.get_G(),
+							ParameterGetter.get_P(), 
+							ParameterGetter.get_S(), 
+							ParameterGetter.get_M(), 
+							ParameterGetter.get_C() 
+							)
+							.outFile("AI17.txt"); 
 		///JUST A TEST CLASS AND GO..
 	}
 	
 }
 
-class ParameterSetter{
-	public static void setParas(int G, int P, int S, int M, int C ){
+class ParameterGetter{
+	
+	private static int G, P, S, M, C ;
+	
+	public static void getUserInput(){		
+		Scanner in = new Scanner(System.in);
 		
-		// Scanner in = new Scanner(System.in);
-		// System.out.println(" G P S M C");
-		// G_GenetaionsNum = in.nextInt();
-		
+		try{
+			System.out.println(">>Enter the number of Genetations:\t");
+			G = Integer.parseInt( in.nextLine() );
+			
+			System.out.println(">>Enter the Population size:\t");
+			P = Integer.parseInt( in.nextLine() );
+			
+			System.out.println(">>Enter the number of Students:\t");
+			S = Integer.parseInt( in.nextLine() );
+			
+			System.out.println(">>Enter The Total number of Modules:\t");
+			M = Integer.parseInt( in.nextLine() );
+			
+			System.out.println(">>Enter The number of Modules in a Course:\t");
+			C = Integer.parseInt( in.nextLine() );
+		}
+		catch(Exception e){	// if $#@ inputed.
+			//e.printStackTrace();
+			System.out.println("***Please Enter a positive Integer!***\n");
+			ParameterGetter.getUserInput( );		// BUG When Ctrl+C.
+		}
+		finally{
+			if( G > 0 && P > 0 && S > 0 && M > 0 && C > 0 ) {}
+			else { 
+				System.out.println("***Please Enter a positive Integer!***\n");
+				ParameterGetter.getUserInput(); 
+			}
+			in.close();
+		}
+	}
+	//*** getter method***//
+	public static int get_G(){
+		return G;
+	}
+	public static int get_P(){
+		return P;
+	}
+	public static int get_S(){
+		return S;
+	}
+	public static int get_M(){
+		return M;
+	}
+	public static int get_C(){
+		return C;
 	}
 }
 
@@ -26,33 +84,92 @@ class GenerateData{
 	int S_StudentNum;
 	int M_TotalModules;
 	int C_inCourseModules;
+	int D_ExamDays;				/* What if total Modules is an odd? SEE GenerateData Constructor*/
 	
-	int [][] StuTimetable;
-	int [][] ExamTimetable;
-	
+	int [][] StuTimetable;		// The first record from index ZERO 0 rather than 1 !
+	int [][][] ExamTimetable;		
+								
 	float [] costFxResult_Arr;
-	
-	GenerateData(){}
-	
+		
 	GenerateData(int G, int P, int S, int M, int C){
 		G_GenetaionsNum		= G;
 		P_PopulationSize	= P;
 		S_StudentNum		= S;
 		M_TotalModules		= M;
 		C_inCourseModules	= C;
+		
+		//### D_ExamDays!!!
+		if( M_TotalModules % 2 == 0 )
+			D_ExamDays = M_TotalModules/2;
+		else
+			D_ExamDays = M_TotalModules/2 + 1;
+		//### END
+		
+		generate_StuTimetable();
+		generate_ExamTimetable();
+		
 	}
 	
 	public void outFile(String fileName){
-		
+		try{
+			File file = new File(fileName);	//read target file 
+			if( !file.exists() ){
+				System.out.println("File: "+ fileName +" does not exist!....\nCreating....\n");
+				file.createNewFile();
+				System.out.println("File is created now, which is on the dir:  "+ file.getAbsolutePath());
+			}
+			//BufferedOutputStream out = new BufferedOutputStream( new FileOutputStream(file));
+			PrintWriter out = new PrintWriter(file);
+			
+			//*** Student Timetable Output ***
+			for(int stu_index = 1; stu_index <= S_StudentNum ; stu_index++){
+				out.print("Student "+ stu_index +":");
+				for(int module_index = 0 ; module_index < C_inCourseModules; module_index++){
+					out.print( " M" + StuTimetable[stu_index - 1][module_index] );
+				}
+				out.println();
+			}
+			
+			out.println();
+			
+			//*** Exam Timetable Output ***
+			for(int ord_index = 1; ord_index <= P_PopulationSize ; ord_index++){
+				
+				out.print("Ord "+ ord_index +":");
+				
+				for(int sessionPart = 0; sessionPart < 2 ; sessionPart ++){
+				
+					for(int module_index = 1 ; module_index <= D_ExamDays; module_index++){
+						out.print( " m" + ExamTimetable[ord_index - 1][sessionPart][module_index - 1] );
+					}
+
+					out.println();
+				}
+
+				out.println(" : cost: " + costFxResult_Arr[ord_index - 1]);
+			}
+			
+			//***END output
+			
+			out.close();
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+		//finally ?
 	}
 	
 	private /*public*/ void generate_StuTimetable(){
-		//assert( S_StudentNum != 0 && C_inCourseModules != 0 );
-		//stuTimetable = new int[S_StudentNum][C_inCourseModules];
+		StuTimetable = new int[S_StudentNum][C_inCourseModules];
+		int index = 0;
+		while( index < S_StudentNum){
+			StuTimetable[index] = Algo.generate_AStuTimeTable( C_inCourseModules, M_TotalModules ) ;
+			index++;
+		}
 	}
 	
 	private /*public*/ void generate_ExamTimetable(){
-		
+		ExamTimetable = new int[P_PopulationSize][2][ D_ExamDays ];
 	}
 	
  
@@ -63,23 +180,25 @@ class Fx_fitness{
 		
 	}
 }
-	
-	
+
+
 // POOR DESIGN
-class RandomSelectionHelper{
+class Algo{
 	boolean ModuleState_Arr[] ; // The module has been chosed in table is TRUE, stand for Do NOT make this number into Account. 
 	public static boolean HasBeenGenerated(){
-		return ;
+		return true;
+	}
+	
+	public static int [] generate_AStuTimeTable(int C_inCourseModules, int M_TotalModules){
+		return null;
 	}
 	
 	public int getAProperExam(){
-		return ;
+		return 1;
 	}
 	
 	//The following is a poor method in name , which is same as the boolean varable array in data field.
 	private int randomNumGenerate(){
-		return ;
+		return 1;
 	}
 }
-
-
